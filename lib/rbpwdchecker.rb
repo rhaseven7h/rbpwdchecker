8fd6072f2f24d3414b4a8b3149469d4ca9cfcff2
Gabriel Medina <gmedina@neptuno.(none)>
2010-05-18 15:42:26 -0600

    Upped the version to 0.0.2

--- lib/rbpwdchecker.rb
+++ lib/rbpwdchecker.rb
@@ -4,5 +4,5 @@ $:.unshift(File.dirname(__FILE__)) unless
 require File.join(File.dirname(__FILE__), '..', 'ext', 'rbpwdchecker', 'rbpwdchecker') if File.exists?(File.join(File.dirname(__FILE__), '..', 'ext', 'rbpwdchecker', 'rbpwdchecker.so'))
 
 module Rbpwdchecker
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
 end
